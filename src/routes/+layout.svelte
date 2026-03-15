82a0a92ffd106a5e4811d14213a1d1704c14792d
Mal <minimal.spacing.a@gmail.com>
2026-03-15 14:27:30 +0000

    adding custom font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
   import Topbar from "./Topbar.svelte";
   import "./style.css";
-  import "fluent-svelte/theme.css";
+  import "./font.css";
 </script>
 
 <div class="flex flex-col h-screen bg-stone-950 text-stone-200">
-  <Topbar />
 
   <div class="flex flex-1 overflow-hidden">
+    <Topbar />
+
     <slot />
   </div>
 </div>
