e555eb04f4d82bb83d7f80899a4f6557a2c1871a
Mal <minimal.spacing.a@gmail.com>
2026-03-16 15:14:48 +0000

    add window commands for managing application windows

---
+++ src-tauri/src/commands/window.rs
@@ -0,0 +1,62 @@
+
+use tauri::command;
+use tauri::Manager;
+
+#[command] // THIS HAVE TO BE ASYNC otherwise its will freezes
+pub async fn open_main_window(app: tauri::AppHandle, url: &str) -> Result<(), String> {
+    let label = "CrushMainWindow";
+
+    if let Some(window) = app.get_webview_window(label) {
+        window.show().map_err(|e| e.to_string())?;
+        window.set_focus().map_err(|e| e.to_string())?;
+        return Ok(());
+    }
+
+    let url = tauri::WebviewUrl::App(url.parse().unwrap());
+
+    tauri::WebviewWindowBuilder::new(&app, label, url)
+        .title("Crush")
+        .closable(true)
+        .inner_size(1000.0, 600.0)
+        .center()
+        .decorations(false)
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+
+#[command] // THIS HAVE TO BE ASYNC otherwise its will freezes
+pub async fn open_choice_window(app: tauri::AppHandle) -> Result<(), String> {
+    let label = "crushBoostrapChoiceWindow"; // trying to be diffrent here | EDIT : dont
+
+    if let Some(window) = app.get_webview_window(label) {
+        window.show().map_err(|e| e.to_string())?;
+        window.set_focus().map_err(|e| e.to_string())?;
+        return Ok(());
+    }
+
+    let url = tauri::WebviewUrl::App("choiceWin".parse().unwrap());
+
+    tauri::WebviewWindowBuilder::new(&app, label, url)
+        .title("crushBoostrapChoiceWindow")
+        .closable(true)
+        .inner_size(500.0, 250.0)
+        .min_inner_size(500.0, 250.0)
+        .center()
+        .decorations(false)
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+
+#[tauri::command]
+pub async fn kill_window(app: tauri::AppHandle, window_name: &str) -> Result<(), String> {
+    if let Some(window) = app.get_webview_window(window_name) {
+        window.close().map_err(|e| e.to_string())?;
+    }
+    Ok(())
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,69 +1,14 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-use tauri::Manager;
 use tauri_plugin_dialog::DialogExt;
+use commands::window::{ open_choice_window, open_main_window, kill_window };
+mod commands;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn kill_window(app: tauri::AppHandle, window_name: &str) -> Result<(), String> {
-    if let Some(window) = app.get_webview_window(window_name) {
-        window.close().map_err(|e| e.to_string())?;
-    }
-    Ok(())
-}
-
-#[tauri::command] // THIS HAVE TO BE ASYNC otherwise its will freezes
-async fn open_main_window(app: tauri::AppHandle, url: &str) -> Result<(), String> {
-    let label = "CrushMainWindow";
-
-    if let Some(window) = app.get_webview_window(label) {
-        window.show().map_err(|e| e.to_string())?;
-        window.set_focus().map_err(|e| e.to_string())?;
-        return Ok(());
-    }
-
-    let url = tauri::WebviewUrl::App(url.parse().unwrap());
-
-    tauri::WebviewWindowBuilder::new(&app, label, url)
-        .title("Crush")
-        .closable(true)
-        .inner_size(1000.0, 600.0)
-        .center()
-        .decorations(false)
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    Ok(())
-}
-
-#[tauri::command] // THIS HAVE TO BE ASYNC otherwise its will freezes
-async fn open_choice_window(app: tauri::AppHandle) -> Result<(), String> {
-    let label = "crushBoostrapChoiceWindow"; // trying to be diffrent here | EDIT : dont
-
-    if let Some(window) = app.get_webview_window(label) {
-        window.show().map_err(|e| e.to_string())?;
-        window.set_focus().map_err(|e| e.to_string())?;
-        return Ok(());
-    }
-
-    let url = tauri::WebviewUrl::App("choiceWin".parse().unwrap());
-
-    tauri::WebviewWindowBuilder::new(&app, label, url)
-        .title("crushBoostrapChoiceWindow")
-        .closable(true)
-        .inner_size(500.0, 250.0)
-        .min_inner_size(500.0, 250.0)
-        .center()
-        .decorations(false)
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    Ok(())
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
