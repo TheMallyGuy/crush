0ad76dd0d3398e5085a5b04ac8ed942ddfe95ef8
Mal <minimal.spacing.a@gmail.com>
2026-03-16 15:38:28 +0000

    fix content slot thinggy

--- src/routes/mainWin/Ui/+layout.svelte
+++ src/routes/mainWin/Ui/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-    import Sidebar from "$lib/components/Sidebar.svelte";
+  import Sidebar from "$lib/components/Sidebar.svelte";
 </script>
 
-<Sidebar/>
+<div class="flex h-screen">
+  <Sidebar />
+
+  <main class="flex-1 overflow-auto bg-[#0f1114] text-white p-6">
+    <slot />
+  </main>
+</div>
