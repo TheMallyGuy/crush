313211e13c4d1e853f7edf31e6984b7fe2a1266b
Mal <minimal.spacing.a@gmail.com>
2026-03-17 10:51:44 +0000

    refactor layout and styles for improved responsiveness and scrollbar visibility

--- src/routes/mainWin/Ui/+layout.svelte
+++ src/routes/mainWin/Ui/+layout.svelte
@@ -2,10 +2,11 @@
   import Sidebar from "$lib/components/Sidebar.svelte";
 </script>
 
-<div class="flex h-screen">
+<div class="flex h-screen w-screen overflow-hidden bg-black text-white">
   <Sidebar />
 
-  <main class="flex-1 overflow-auto text-white p-6">
+  <main class="flex-1 flex flex-col items-stretch min-w-0 overflow-y-auto p-10 gap-6">
     <slot />
   </main>
 </div>
+
