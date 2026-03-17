371094fd7ea5050ec35c694e1c57defe6a88aa23
Mal <minimal.spacing.a@gmail.com>
2026-03-17 10:51:50 +0000

    test route

---
+++ src/routes/mainWin/Ui/installation/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { invoke } from '@tauri-apps/api/core';
+  import SettingCard from '$lib/components/SettingCard.svelte';
+  import { Balloon } from '@lucide/svelte';
+
+    async function openmainwin() {
+        await invoke("open_choice_window");
+
+        setTimeout(() => { // wait before killing to prevent crash
+            invoke("kill_window", { windowName: "CrushMainWindow" });
+        }, 100);
+    }
+</script>
+
+<h1 class="text-4xl font-bold text-blue-500">
+    something that very cool and tuff too
+</h1>
+
+<button on:click={openmainwin}>
+  open choice win
+</button>
+
+<div class="flex-col flex gap-4">
+    <SettingCard title="do balls" description="balls are good" icon={Balloon}>
+    </SettingCard>
+    <SettingCard title="do balls" description="balls are good" icon={Balloon}>
+    </SettingCard>
+    <SettingCard title="do balls" description="balls are good" icon={Balloon}>
+    </SettingCard>
+    <SettingCard title="do balls" description="balls are good" icon={Balloon}>
+    </SettingCard>
+</div>
