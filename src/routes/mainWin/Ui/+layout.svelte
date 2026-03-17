195f239996ed7e58930c2107c099d5e89469518e
Mal <minimal.spacing.a@gmail.com>
2026-03-17 06:12:30 +0000

    delete color

--- src/routes/mainWin/Ui/+layout.svelte
+++ src/routes/mainWin/Ui/+layout.svelte
@@ -5,7 +5,7 @@
 <div class="flex h-screen">
   <Sidebar />
 
-  <main class="flex-1 overflow-auto bg-[#0f1114] text-white p-6">
+  <main class="flex-1 overflow-auto text-white p-6">
     <slot />
   </main>
 </div>
