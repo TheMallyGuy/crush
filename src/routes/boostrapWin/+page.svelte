7e7e43549dbfd54577f90d497782d8525a67ebf5
Mal <minimal.spacing.a@gmail.com>
2026-03-20 11:52:04 +0000

    use this instead of that

--- src/routes/boostrapWin/+page.svelte
+++ src/routes/boostrapWin/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { invoke } from '@tauri-apps/api/core'
+    import { downloadRoblox } from '$lib/downloadRoblox';
 
     let description = 'Preveiii!!'
 
@@ -36,7 +37,7 @@
     <div class="absolute bottom-6 left-0 w-full flex justify-center p-3">
         <div class="w-full max-w-sm">
             <button
-                on:click={changeDescription}
+                on:click={downloadRoblox}
                 // later change to real code at here!!
                 class="w-full bg-stone-900 hover:bg-stone-800 active:scale-[0.98] disabled:opacity-50 rounded-lg p-4 flex items-center justify-center gap-3 transition-all border border-stone-800 hover:border-stone-700 text-stone-200"
             >
