1c3b9853366c03a1ecabe2d369e46f55f40b58f2
Mal <minimal.spacing.a@gmail.com>
2026-03-21 08:23:06 +0000

    add auto update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
     import { onMount } from 'svelte'
     import { loadSavedTheme } from '$lib/theme/themeLoader'
+    import { check } from '@tauri-apps/plugin-updater';
 
     onMount(async () => {
         await loadSavedTheme()
+        let update = await check()
+
+        if (update) {
+            await update.downloadAndInstall()
+        }
+
     })
 </script>
 
