707599eb1a48ba99406867c4f1384d8bcfb39e13
Mal <minimal.spacing.a@gmail.com>
2026-03-22 02:24:42 +0000

    card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let clickable = false
+
+    let className = ''
+    export { className as class }
+</script>
+
+<div
+    class="group relative flex w-full flex-col rounded-xl bg-[#0f0f0f] p-5 transition-all duration-200
+  {clickable ? 'cursor-pointer hover:bg-[#161616] active:scale-[0.995]' : ''} 
+  {className}"
+    {...$$restProps}
+>
+    <slot />
+</div>
