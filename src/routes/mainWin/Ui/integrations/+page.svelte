e0f5dc6eec4d8dad4c556e162502387773b2028c
Mal <minimal.spacing.a@gmail.com>
2026-03-27 11:03:28 +0000

    page without functions

--- src/routes/mainWin/Ui/integrations/+page.svelte
+++ src/routes/mainWin/Ui/integrations/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
     import SettingCard from "$lib/components/SettingCard.svelte";
+    import Switch from "$lib/components/Switch.svelte"
+    import { Bell } from "@lucide/svelte";
     import { invoke } from "@tauri-apps/api/core"
     import { onMount } from "svelte";
 
     onMount(async () => {
         await invoke("set_rpc", { details: "A roblox boostrapper written from scratch", stateText: "In Intergrations Route" })
     })
+
+    function handleChanges() {
+        // doesnt do anything yet
+    }
 
 </script>
 
@@ -17,6 +23,20 @@
             </h1>
             <p class="text-stone-400 mt-1">Config intergrations outside roblox</p>
         </div>
+    </div>
 
+    <div class="flex flex-col gap-3">
+        <SettingCard
+            title="Server Location Notifier"
+            description="Get notify when client connect to a server."
+        >
+            <Switch slot="action"/>
+        </SettingCard>
+            <SettingCard
+            title="Discord RPC (Crush)"
+            description="Replace the Roblox Rich Presence with Crush's"
+        >
+            <Switch slot="action"/>
+        </SettingCard>
     </div>
 </div>
