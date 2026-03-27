98e32fe7316dff0714c9a00951c1d4c6b9b153d7
Mal <minimal.spacing.a@gmail.com>
2026-03-27 11:24:19 +0000

    actually functioning page

--- src/routes/mainWin/Ui/integrations/+page.svelte
+++ src/routes/mainWin/Ui/integrations/+page.svelte
@@ -1,18 +1,56 @@
 <script lang="ts">
     import SettingCard from "$lib/components/SettingCard.svelte";
-    import Switch from "$lib/components/Switch.svelte"
-    import { Bell } from "@lucide/svelte";
-    import { invoke } from "@tauri-apps/api/core"
+    import Switch from "$lib/components/Switch.svelte";
+    import { invoke } from "@tauri-apps/api/core";
     import { onMount } from "svelte";
+    import { load } from "@tauri-apps/plugin-store";
 
-    onMount(async () => {
-        await invoke("set_rpc", { details: "A roblox boostrapper written from scratch", stateText: "In Intergrations Route" })
-    })
-
-    function handleChanges() {
-        // doesnt do anything yet
+    type Intergrations = {
+        crushRpc: boolean,
+        serverLocationNotifier: boolean
     }
 
+    type Config = {
+        FirstLaunch: string,
+        bestRegion: string,
+        intergrations: Intergrations
+    }
+
+    let crushRpc = false;
+    let serverLocationNotifier = false;
+
+    async function loadConfig() {
+        const store = await load("config.json");
+        const savedIntergrations = await store.get<Intergrations>("intergrations");
+        
+        if (savedIntergrations) {
+            crushRpc = savedIntergrations.crushRpc;
+            serverLocationNotifier = savedIntergrations.serverLocationNotifier;
+        }
+    }
+
+    onMount(async () => {
+        await loadConfig(); 
+
+        await invoke("set_rpc", { 
+            details: "A roblox bootrapper written from scratch", 
+            stateText: "In Intergrations Route" 
+        });
+    });
+
+    async function handleChanges() {
+        const store = await load("config.json");
+
+
+        const newIntergrations: Intergrations = {
+            crushRpc,
+            serverLocationNotifier
+        };
+
+        await store.set("intergrations", newIntergrations);
+        
+        await store.save(); 
+    }
 </script>
 
 <div class="flex flex-col gap-8 max-w-2xl">
@@ -30,13 +68,13 @@
             title="Server Location Notifier"
             description="Get notify when client connect to a server."
         >
-            <Switch slot="action"/>
+            <Switch slot="action" bind:checked={serverLocationNotifier} on:change={handleChanges}/>
         </SettingCard>
             <SettingCard
             title="Discord RPC (Crush)"
             description="Replace the Roblox Rich Presence with Crush's"
         >
-            <Switch slot="action"/>
+            <Switch slot="action" bind:checked={crushRpc} on:change={handleChanges}/>
         </SettingCard>
     </div>
 </div>
