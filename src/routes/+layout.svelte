825eab5e38e7b2d4cc934a5f3d933bc8948c0121
Mal <minimal.spacing.a@gmail.com>
2026-03-28 04:47:33 +0000

    detect deeplink

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,25 @@
     import { onMount } from 'svelte'
     import { loadSavedTheme } from '$lib/theme/themeLoader'
     import { check } from '@tauri-apps/plugin-updater'
+    import { listen } from "@tauri-apps/api/event"
+    import { deepLinkUrl } from "$lib/stores/deeplink"
+    import { goto } from '$app/navigation'
 
     onMount(async () => {
         await loadSavedTheme()
-        let update = await check()
 
+        await listen<string>("deep-link-received", (event) => {
+            deepLinkUrl.set(event.payload)
+            goto("/boostrapWin")
+        })
+
+        const update = await check()
         if (update) {
             await update.downloadAndInstall()
+            return
         }
+
+        goto("/boostrapWin")
     })
 </script>
 
