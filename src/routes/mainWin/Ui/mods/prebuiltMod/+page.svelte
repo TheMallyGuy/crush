8a03ebf8c5131190fa6550aeced6f22cb366949e
Mal <minimal.spacing.a@gmail.com>
2026-03-30 22:20:10 +0700

    function!

--- src/routes/mainWin/Ui/mods/prebuiltMod/+page.svelte
+++ src/routes/mainWin/Ui/mods/prebuiltMod/+page.svelte
@@ -2,6 +2,10 @@
     import SettingCard from "$lib/components/molecules/SettingCard.svelte";
     import Dropdown from "$lib/components/molecules/Dropdown.svelte";
     import Button from "$lib/components/atoms/Button.svelte";
+    import { customFont } from "$lib/mods/prebuiltMod";
+    import { getCurrentInstallation } from "$lib/downloadRoblox";
+    import { open } from '@tauri-apps/plugin-dialog';
+    import { appDataDir, join } from "@tauri-apps/api/path"
 
     let cursorOptions =  [
         { value: "default", label: "Default" },
@@ -9,6 +13,24 @@
         { value: "2007", label: "2007" }
     ];
     let cursorValue = "default";
+
+    async function handleCustomFont() {
+        const file = await open({
+            multiple: false,
+            filters: [{
+                name: 'Font',
+                extensions: ['ttf']
+            }]
+        });
+        if (!file) return
+
+        const currentInstallation = await getCurrentInstallation()
+        if (!currentInstallation) return
+
+        const rblxFamilies = await join(currentInstallation.installPath, "content", "fonts", "families")
+        customFont(file, rblxFamilies)
+    }
+
 </script>
 
 <div class="flex flex-col gap-3">
@@ -17,6 +39,6 @@
     </SettingCard>
 
     <SettingCard title="Custom Font" description="Change your roblox font to whatever you like.">
-        <Button slot="action" variant='secondary'>Change Font</Button>
+        <Button slot="action" variant='secondary' on:click={handleCustomFont}>Change Font</Button>
     </SettingCard>
 </div>
