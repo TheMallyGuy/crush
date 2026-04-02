85953035b2db08e9da1e29f5d99f1e415c4eddf6
Mal <minimal.spacing.a@gmail.com>
2026-04-02 12:26:11 +0700

    wow

--- src/routes/mainWin/Ui/installation/+page.svelte
+++ src/routes/mainWin/Ui/installation/+page.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
     import { invoke } from '@tauri-apps/api/core'
     import SettingCard from '$lib/components/molecules/SettingCard.svelte'
-    import { Balloon } from '@lucide/svelte'
+    import Textbox from '$lib/components/atoms/Textbox.svelte';
+    import { Balloon, Rocket } from '@lucide/svelte'
     import { onMount } from 'svelte'
+    import { load } from '@tauri-apps/plugin-store'
+
+    let version = ""
+    let config
 
     onMount(async () => {
         await invoke('set_rpc', {
             details: 'A roblox boostrapper written from scratch',
             stateText: 'In Installation Route',
         })
+
+        config = await load("config.json")
     })
+
+    async function handleVersionChange() {
+        
+    }
 </script>
 
 <h1 class="text-4xl font-bold text-white">Installation</h1>
 
-<div class="flex-col flex gap-4"></div>
+<div class="flex flex-col gap-3">
+    <SettingCard title="Version" description="Skip update check and download the version." icon={Rocket}>
+        <Textbox slot="action" class="w-48 h-8 text-sm" bind:value={version} />    
+    </SettingCard>
+</div>
