9a2b0908918ac3a4952e4a911ec2c008ae7029f2
Mal <minimal.spacing.a@gmail.com>
2026-04-03 13:50:45 +0700

    working installation page

--- src/routes/mainWin/Ui/installation/+page.svelte
+++ src/routes/mainWin/Ui/installation/+page.svelte
@@ -6,8 +6,26 @@
     import { onMount } from 'svelte'
     import { load } from '@tauri-apps/plugin-store'
 
-    let version = ""
+    type Installation = {
+        version: string
+    }
+
+    type Config = {
+        installation: Installation
+    }
+
+    let version:string
     let config
+
+    async function loadConfig() {
+        const store = await load('config.json')
+        const savedInstallation =
+            await store.get<Installation>('installation')
+
+        if (savedInstallation) {
+            version = savedInstallation.version ?? 'latest'
+        }
+    }
 
     onMount(async () => {
         await invoke('set_rpc', {
@@ -15,18 +33,26 @@
             stateText: 'In Installation Route',
         })
 
-        config = await load("config.json")
+        await loadConfig()
     })
 
-    async function handleVersionChange() {
-        
+    async function handleChanges() {
+        const store = await load('config.json')
+
+        const newInstallation: Installation = {
+            version,
+        }
+
+        await store.set('installation', newInstallation)
+
+        await store.save()
     }
 </script>
 
 <h1 class="text-4xl font-bold text-white">Installation</h1>
 
 <div class="flex flex-col gap-3">
-    <SettingCard title="Version" description="Skip update check and download the version." icon={Rocket}>
-        <Textbox slot="action" class="w-48 h-8 text-sm" bind:value={version} />    
+    <SettingCard title="Version" description="Skip update check and download the version. The default value is latest. Unvaild version will not get downloaded and will fallback into the latest version." icon={Rocket}>
+        <Textbox slot="action" class="w-48 h-8 text-sm" bind:value={version} on:change={handleChanges} />    
     </SettingCard>
 </div>
