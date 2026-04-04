8db3e50fa4ac8dc7240a962704d36f7304954a77
Mal <minimal.spacing.a@gmail.com>
2026-04-04 21:52:52 +0700

    localize installation

--- src/routes/mainWin/Ui/installation/+page.svelte
+++ src/routes/mainWin/Ui/installation/+page.svelte
@@ -6,6 +6,7 @@
     import { onMount } from 'svelte'
     import { load } from '@tauri-apps/plugin-store'
     import type { Installation } from '$lib/types';
+    import { _ } from 'svelte-i18n';
 
     let version:string
     let config
@@ -45,7 +46,7 @@
 <h1 class="text-4xl font-bold text-white">Installation</h1>
 
 <div class="flex flex-col gap-3">
-    <SettingCard title="Version" description="Skip update check and download the version. The default value is latest. Unvaild version will not get downloaded and will fallback into the latest version." icon={Rocket}>
+    <SettingCard title={$_('installations.customVersion.title')} description={$_('installations.customVersion.description')} icon={Rocket}>
         <Textbox slot="action" class="w-48 h-8 text-sm" bind:value={version} on:change={handleChanges} />    
     </SettingCard>
 </div>
