ea12aad1ae50d606593d35f9ad8f4755f5245d1c
Mal <minimal.spacing.a@gmail.com>
2026-04-08 12:18:59 +0700

    doing it myself

--- src/routes/mainWin/Ui/+layout.svelte
+++ src/routes/mainWin/Ui/+layout.svelte
@@ -1,28 +1,20 @@
 <script lang="ts">
     import Sidebar from '$lib/components/molecules/Sidebar.svelte'
     import LaunchBar from '$lib/components/molecules/LaunchBar.svelte'
-    import { fly } from 'svelte/transition'
-    import { page } from '$app/stores'
+    import { preparePageTransition } from '$lib/page-transition'
+
+    preparePageTransition()
 </script>
 
 <div class="flex h-screen w-screen overflow-hidden bg-black text-white">
     <Sidebar />
-
     <div class="flex-1 flex flex-col min-w-0">
-        <main
-            class="flex-1 relative min-w-0 overflow-y-auto"
-        >
-            {#key $page.url.pathname}
-                <div
-                    in:fly={{ x: 20, duration: 200, delay: 200 }}
-                    out:fly={{ x: -20, duration: 200 }}
-                    class="absolute inset-0 p-10 flex flex-col items-stretch gap-6"
-                >
-                    <slot />
-                </div>
-            {/key}
+        <main class="flex-1 relative min-w-0 overflow-hidden">
+            <div class="absolute inset-0 p-10 flex flex-col items-stretch gap-6 overflow-y-auto"
+                style="view-transition-name: page-content">
+                <slot />
+            </div>
         </main>
-
         <footer class="flex justify-end items-end shrink-0">
             <LaunchBar />
         </footer>
