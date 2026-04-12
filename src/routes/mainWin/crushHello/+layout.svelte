7a49a2555b702082a1bb92abc700aa5d053d34f8
Mal <minimal.spacing.a@gmail.com>
2026-04-12 20:05:55 +0700

    gammar

--- src/routes/mainWin/crushHello/+layout.svelte
+++ src/routes/mainWin/crushHello/+layout.svelte
@@ -20,7 +20,7 @@
 
         await invoke('create_or_focus_window', {
             label: 'CrushMainWindow',
-            url: 'mainWin/Ui/intergrations',
+            url: 'mainWin/Ui/integrations',
             title: 'Crush',
             width: 1000,
             height: 600,
