367c38097d832637600993a9256f175a6e118c43
Mal <minimal.spacing.a@gmail.com>
2026-04-14 17:09:42 +0700

    return the config

--- src-tauri/src/commands/boostrapperimporter.rs
+++ src-tauri/src/commands/boostrapperimporter.rs
@@ -1,27 +1,23 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
 
-#[derive(Deserialize, Debug)]
-struct BloxstrapConfig {
-    CheckForUpdates: bool,
-    BackgroundUpdatesEnabled: bool,
-    UseDiscordRichPresence: bool,
-    ShowServerDetails: bool,
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug)]
+pub struct BloxstrapConfig {
+    pub CheckForUpdates: bool,
+    pub BackgroundUpdatesEnabled: bool,
+    pub UseDiscordRichPresence: bool,
+    pub ShowServerDetails: bool,
 }
 
 #[tauri::command]
-pub async fn export_boostrapconfig(boostrap_config_path: String) -> Result<(), String> {
+pub async fn export_boostrapconfig(boostrap_config_path: String) -> Result<BloxstrapConfig, String> {
     let path = PathBuf::from(boostrap_config_path).join("Settings.json");
-
     let data = fs::read_to_string(&path)
-        .map_err(|e| e.to_string())?;
-
+        .map_err(|e: std::io::Error| e.to_string())?;
     let config: BloxstrapConfig = serde_json::from_str(&data)
-        .map_err(|e| e.to_string())?;
+        .map_err(|e: serde_json::Error| e.to_string())?;
 
-    // print (debug)
-    println!("{:?}", config.BackgroundUpdatesEnabled);
-
-    Ok(())
+    Ok(config)
 }
